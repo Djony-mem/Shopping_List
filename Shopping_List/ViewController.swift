85023eae670ab4548361cbe13763b6f3cbe8942d
Бруйко Евгения <bruyko.e@gmail.com>
2021-01-30 02:06:36 +0300

    Initial Commit

---
+++ Shopping_List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Shopping_List
+//
+//  Created by Brubrusha on 1/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
